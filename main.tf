b0daeed174c7f3b588ad1be4dea3c91b84696164
Peter ONeill <jaretep@gmail.com>
2020-04-03 10:51:36 -0500

    added transcoder role

--- main.tf
+++ main.tf
@@ -6,6 +6,150 @@ provider "aws" {
 variable "prefix" {
   default = "peter-serverless"
 }
-resource "aws_s3_bucket" "serverless" {
-  bucket = "${var.prefix}new-bucket"
+#################
+# S3 buckets
+#################
+resource "aws_s3_bucket" "serverless-upload" {
+  bucket = "${var.prefix}-sfp-upload"
+  force_destroy = true
 }
+resource "aws_s3_bucket" "serverless-transcode" {
+  bucket = "${var.prefix}-sfp-transcode"
+  force_destroy = true
+}
+resource "aws_s3_bucket_public_access_block" "serverless" {
+  bucket = aws_s3_bucket.serverless-transcode.id
+
+  block_public_acls   = true
+  ignore_public_acls = true
+  block_public_policy = false
+  restrict_public_buckets = false
+}
+resource "aws_s3_bucket_policy" "b" {
+  bucket = aws_s3_bucket.serverless-transcode.id
+
+  policy = <<POLICY
+{
+	"Version": "2012-10-17",
+	"Statement": [
+		{
+			"Sid": "AddPerm",
+			"Effect": "Allow",
+			"Principal": "*",
+			"Action": "s3:GetObject",
+			"Resource": "arn:aws:s3:::${aws_s3_bucket.serverless-transcode.bucket}/*"
+		}
+	]
+}
+POLICY
+}
+  
+#################
+# IAM lambda role
+#################
+resource "aws_iam_role" "serverless" {
+  name = "${var.prefix}-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_lambda.json
+}
+data "aws_iam_policy_document" "assume_lambda" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+resource "aws_iam_role_policy_attachment" "lambda_execute" {
+  role       = aws_iam_role.serverless.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSLambdaExecute"
+}
+resource "aws_iam_role_policy_attachment" "elastic_jobs_submitter" {
+  role       = aws_iam_role.serverless.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonElasticTranscoder_JobsSubmitter"
+}
+#################
+# IAM Transcoder Role
+#################
+resource "aws_iam_role" "transcoder" {
+  name = "${var.prefix}-transcoder-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_elastictranscoder.json
+}
+data "aws_iam_policy_document" "assume_elastictranscoder" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["elastictranscoder.amazonaws.com"]
+    }
+  }
+}
+resource "aws_iam_policy" "elastictranscoder" {
+  name        = "${var.prefix}-elastictranscoder-default"
+  path        = "/"
+  description = "Copy of ElasticTranscoder default role"
+
+  policy = <<EOF
+{
+   "Version":"2012-10-17",
+   "Statement":[
+      {
+         "Sid":"1",
+         "Effect":"Allow",
+         "Action":[
+            "s3:Get*",
+            "s3:ListBucket",
+            "s3:Put*",
+            "s3:*MultipartUpload*"
+         ],
+         "Resource":"*"
+      },
+      {
+         "Sid":"2",
+         "Effect":"Allow",
+         "Action":"sns:Publish",
+         "Resource":"*"
+      },
+      {
+         "Sid":"3",
+         "Effect":"Deny",
+         "Action":[
+            "sns:*Permission*",
+            "sns:*Delete*",
+            "sns:*Remove*",
+            "s3:*Policy*",
+            "s3:*Delete*"
+         ],
+         "Resource":"*"
+      }
+   ]
+}
+EOF
+}
+resource "aws_iam_role_policy_attachment" "elastictranscoder" {
+  role       = aws_iam_role.serverless.name
+  policy_arn = aws_iam_policy.elastictranscoder.arn
+}
+
+#################
+# Elastic Transcoder
+#################
+# resource "aws_elastictranscoder_pipeline" "serverless" {
+#   input_bucket = "${aws_s3_bucket.input_bucket.bucket}"
+#   name         = "${var.prefix}-serverless-pipeline"
+#   role         = "${aws_iam_role.test_role.arn}"
+
+#   content_config {
+#     bucket        = "${aws_s3_bucket.content_bucket.bucket}"
+#     storage_class = "Standard"
+#   }
+
+#   thumbnail_config {
+#     bucket        = "${aws_s3_bucket.thumb_bucket.bucket}"
+#     storage_class = "Standard"
+#   }
+# }
+
+
