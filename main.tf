6cca779788d1c839a871131739ab7a38133e9bf2
Peter ONeill <jaretep@gmail.com>
2020-04-04 08:12:38 -0500

    added auth-

--- main.tf
+++ main.tf
@@ -1,15 +1,19 @@
 provider "aws" {
-  region                  = "us-west-2"
+  region                  = var.region
   shared_credentials_file = "~/.aws/credentials"
   profile                 = "personal"
 }
 variable "prefix" {
   default = "peter-serverless"
 }
+variable "region" {
+  default = "us-west-2"
+}
+
 #################
 # S3 buckets
 #################
-resource "aws_s3_bucket" "serverless-upload" {
+resource "aws_s3_bucket" "serverless_upload" {
   bucket = "${var.prefix}-sfp-upload"
   force_destroy = true
 }
@@ -47,9 +51,11 @@ POLICY
 #################
 # IAM lambda role
 #################
-resource "aws_iam_role" "serverless" {
+resource "aws_iam_role" "serverless_lambda" {
   name = "${var.prefix}-role"
   assume_role_policy = data.aws_iam_policy_document.assume_lambda.json
+
+  force_detach_policies = true
 }
 data "aws_iam_policy_document" "assume_lambda" {
   statement {
@@ -62,11 +68,11 @@ data "aws_iam_policy_document" "assume_lambda" {
   }
 }
 resource "aws_iam_role_policy_attachment" "lambda_execute" {
-  role       = aws_iam_role.serverless.name
+  role       = aws_iam_role.serverless_lambda.name
   policy_arn = "arn:aws:iam::aws:policy/AWSLambdaExecute"
 }
 resource "aws_iam_role_policy_attachment" "elastic_jobs_submitter" {
-  role       = aws_iam_role.serverless.name
+  role       = aws_iam_role.serverless_lambda.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonElasticTranscoder_JobsSubmitter"
 }
 #################
@@ -75,6 +81,8 @@ resource "aws_iam_role_policy_attachment" "elastic_jobs_submitter" {
 resource "aws_iam_role" "transcoder" {
   name = "${var.prefix}-transcoder-role"
   assume_role_policy = data.aws_iam_policy_document.assume_elastictranscoder.json
+
+  force_detach_policies = true
 }
 data "aws_iam_policy_document" "assume_elastictranscoder" {
   statement {
@@ -129,7 +137,7 @@ resource "aws_iam_policy" "elastictranscoder" {
 EOF
 }
 resource "aws_iam_role_policy_attachment" "elastictranscoder" {
-  role       = aws_iam_role.serverless.name
+  role       = aws_iam_role.transcoder.name
   policy_arn = aws_iam_policy.elastictranscoder.arn
 }
 
@@ -139,11 +147,73 @@ resource "aws_iam_role_policy_attachment" "elastictranscoder" {
 resource "aws_elastictranscoder_pipeline" "serverless" {
   name         = "${var.prefix}-serverless-pipeline"
 
-  input_bucket = aws_s3_bucket.serverless-upload.bucket
+  input_bucket = aws_s3_bucket.serverless_upload.bucket
   output_bucket = aws_s3_bucket.serverless-transcode.bucket
 
   role         = aws_iam_role.transcoder.arn
 
 }
+#################
+# Lambda Function
+#################
+resource "aws_lambda_function" "serverless_lambda" {
+  filename      = "./docker/lab1/Lambda-Deployment.zip"
+  function_name = "${var.prefix}-lambda-function"
+  role          = aws_iam_role.serverless_lambda.arn
+  handler       = "index.handler"
 
+  source_code_hash = filebase64sha256("./docker/lab1/Lambda-Deployment.zip")
 
+  runtime = "nodejs12.x"
+  timeout = 30
+
+  environment {
+    variables = {
+      ELASTIC_TRANSCODER_REGION = var.region
+      ELASTIC_TRANSCODER_PIPELINE_ID = aws_elastictranscoder_pipeline.serverless.id
+    }
+  }
+}
+#################
+# Lambda Trigger
+#################
+resource "aws_lambda_permission" "allow_serverless_upload" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.serverless_lambda.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.serverless_upload.arn
+}
+
+resource "aws_s3_bucket_notification" "serverless_upload" {
+  bucket = aws_s3_bucket.serverless_upload.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.serverless_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_serverless_upload]
+}
+#################
+# Auth0
+#################
+provider "auth0" {
+  domain=var.AUTH0_DOMAIN
+  client_id=var.AUTH0_CLIENT_ID
+  client_secret=var.AUTH0_CLIENT_SECRET
+}
+
+variable  "AUTH0_DOMAIN" {}
+variable  "AUTH0_CLIENT_ID" {}
+variable  "AUTH0_CLIENT_SECRET" {}
+
+#################
+# Lab 2
+#################
+provider "local" {}
+
+resource "local_file" "website_config" {
+    content     =  templatefile("./docker/lab2/js/config_template.js", { domain = var.AUTH0_DOMAIN, clientId = var.AUTH0_CLIENT_ID })
+    filename = "./docker/lab2/js/config.js"
+}
